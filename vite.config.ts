9099001746e0471592d4fa02dabdce4a700ea9f1
askazz44 <ascaniovecchio06@gmail.com>
2025-11-08 17:51:01 +0100

    Update PWA icon types to 'image/jpeg'
    Changed icon type from 'image/png' to 'image/jpeg' for PWA icons.

--- vite.config.ts
+++ vite.config.ts
@@ -30,13 +30,13 @@ export default defineConfig(({ mode }) => ({
           {
             src: 'pwa-icon-192.png',
             sizes: '192x192',
-            type: 'image/png',
+            type: 'image/jpeg',
             purpose: 'any maskable'
           },
           {
             src: 'pwa-icon-512.png',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/jpeg',
             purpose: 'any maskable'
           }
         ]
